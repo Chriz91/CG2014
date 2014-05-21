54ef358c7321e68c904eb5c77aa3f0cdbf7b39a9
Eremeos <Matthias.Bergler@gmx.de>
2014-05-21 21:02:48 +0200

    Alle Formen + Verschiedene Farben

--- Tetris/Tetrominoes.js
+++ Tetris/Tetrominoes.js
@@ -13,16 +13,82 @@ var ShapeEnum = {
 var Shape = function()
 {
 	
-
+	this.ColorArray = [
+	0xf0fff0,
+	0xf00faa,
+	0xf0ff00,
+	0x00ff00,
+	0xff8c00,
+	0x0000aa,
+	0xff0000,
+	                ];
 	
 	this.ShapeArray = [
+	                   //Lineshape
+	                   [
 	                   [0, 0],
 	                   [0, 1],
 	                   [0, 2],
 	                   [0, 3],
+	                   ],
+	                   //Quadrat
+	                   [
+		                [0, 0],
+		                [0, 1],
+		                [1, 0],
+		                [1, 1],
+		                ],
+	                   //T-Form
+		                [
+		                 [0, 0],
+			                [-1, 1],
+			                [0, 1],
+			                [1, 1],
+			                ],
+			                // Z-Form
+			                   [
+				                [0, 0],
+				                [-1, 1],
+				                [1, 0],
+				                [0, 1],
+				                ],	
+				                
+				                //S-Form
+				                   [
+					                [0, 0],
+					                [1, 1],
+					                [-1, 0],
+					                [0, 1],
+					                ],	
+					                //L-Form
+					                   [
+						                [0, 0],
+						                [1, 0],
+						                [0, 2],
+						                [0, 1],
+						                ],	
+						                
+						                //Mirrowed L
+						                   [
+							                [0, 0],
+							                [-1, 0],
+							                [0, 2],
+							                [0, 1],
+							                ],	
+							                
 	                   ];
 	
-	var material = new THREE.MeshBasicMaterial( {color: 0xf0fff0, side: THREE.DoubleSide} ); 
+	this.setNumber= function()
+	{
+		this.number = Math.floor((Math.random()*7));
+	};
+	 this.setNumber();
+	this.getNumber= function()
+	{
+		return this.number;
+	};
+	
+	var material = new THREE.MeshBasicMaterial( {color: this.ColorArray[this.getNumber()], side: THREE.DoubleSide} ); 
 
 	var block1 = new THREE.Mesh(CreateGeometry(),material);
 	var block2 = new THREE.Mesh(CreateGeometry(),material);
@@ -30,16 +96,17 @@ var Shape = function()
 	var block4 = new THREE.Mesh(CreateGeometry(),material);
 
 	
+
 	  this.setShape = function() {
-		  var number= Math.floor((Math.random()*7));
-		  block1.position.x = this.ShapeArray[0][0];
-		  block1.position.y = this.ShapeArray[0][1];
-		  block2.position.x = this.ShapeArray[1][0];
-		  block2.position.y = this.ShapeArray[1][1];
-		  block3.position.x = this.ShapeArray[2][0];
-		  block3.position.y = this.ShapeArray[2][1];
-		  block4.position.x = this.ShapeArray[3][0];
-		  block4.position.y = this.ShapeArray[3][1];
+		  var number= this.getNumber();
+		  this.getBlock1().position.x = this.ShapeArray[number][0][0];
+		  this.getBlock1().position.y = this.ShapeArray[number][0][1];
+		  this.getBlock2().position.x = this.ShapeArray[number][1][0];
+		  this.getBlock2().position.y = this.ShapeArray[number][1][1];
+		  this.getBlock3().position.x = this.ShapeArray[number][2][0];
+		  this.getBlock3().position.y = this.ShapeArray[number][2][1];
+		  this.getBlock4().position.x = this.ShapeArray[number][3][0];
+		  this.getBlock4().position.y = this.ShapeArray[number][3][1];
 		  };
 	
 		  
@@ -52,10 +119,10 @@ var Shape = function()
 		};
 		
 		this.setY = function(y){
-			block1.position.y += y;
-			block2.position.y += y;
-			block3.position.y += y;
-			block4.position.y += y;
+			this.getBlock1().position.y += y;
+			this.getBlock2().position.y += y;
+			this.getBlock3().position.y += y;
+			this.getBlock4().position.y += y;
 			
 		};
 		
@@ -85,178 +152,6 @@ var Shape = function()
 	return this;
 };
 
-function CreateMirroredLShape()
-{
-	
-  var plane = CreateGeometry();
-  var plane1 = CreateGeometry();
-  var plane2 = CreateGeometry();
-  var plane3 = CreateGeometry();
-  var plane4 = CreateGeometry();
-  var material = new THREE.MeshBasicMaterial( {color: 0xf0fff0, side: THREE.DoubleSide} ); 
-
-  var mesh1 = new THREE.Mesh( plane1 );
-  mesh1.position.x= 0;
-  mesh1.position.y = 0;
-  THREE.GeometryUtils.merge(plane, mesh1);
-
-  var mesh2 = new THREE.Mesh( plane2 );
-  mesh2.position.x = mesh1.position.x+1;
-  THREE.GeometryUtils.merge(plane, mesh2);
-
-  var mesh3 = new THREE.Mesh( plane3 );
-  mesh3.position.x = mesh2.position.x;
-  mesh3.position.y = mesh2.position.y+1;
-  THREE.GeometryUtils.merge(plane, mesh3);
-
-  var mesh4 = new THREE.Mesh(plane4);
-  mesh4.position.x = mesh3.position.x;
-  mesh4.position.y = mesh3.position.y+1;
-  THREE.GeometryUtils.merge(plane, mesh4);
-
-  mesh = new THREE.Mesh( plane, material );
-
-  return mesh;
-}
-
-
-function CreateLShape()
-{
-  var plane = CreateGeometry();
-  var plane1 = CreateGeometry();
-  var plane2 = CreateGeometry();
-  var plane3 = CreateGeometry();
-  var plane4 = CreateGeometry();
-  var material = new THREE.MeshBasicMaterial( {color: 0xf00faa, side: THREE.DoubleSide} ); 
-
-  var mesh1 = new THREE.Mesh( plane1 );
-  mesh1.position.x= 0;
-  mesh1.position.y = 0;
-  THREE.GeometryUtils.merge(plane, mesh1);
-
-  var mesh2 = new THREE.Mesh( plane2 );
-  mesh2.position.y = mesh1.position.y+1;
-  THREE.GeometryUtils.merge(plane, mesh2);
-
-  var mesh3 = new THREE.Mesh( plane3 );
-  mesh3.position.y = mesh2.position.y+1;
-  THREE.GeometryUtils.merge(plane, mesh3);
-
-  var mesh4 = new THREE.Mesh(plane4);
-  mesh4.position.x = mesh3.position.x+1;
-  mesh4.position.y = mesh1.position.y;
-  THREE.GeometryUtils.merge(plane, mesh4);
-
-  mesh = new THREE.Mesh( plane, material );
-
-  return mesh;
-}
-
-
-function CreateTShape()
-{	
-  var plane = CreateGeometry();
-  var plane1 = CreateGeometry();
-  var plane2 = CreateGeometry();
-  var plane3 = CreateGeometry();
-  var plane4 = CreateGeometry();
-  var material = new THREE.MeshBasicMaterial( {color: 0xf0ff00, side: THREE.DoubleSide} ); 
-
-  var mesh1 = new THREE.Mesh( plane1 );
-  mesh1.position.x= 0;
-  mesh1.position.y = 0;
-  THREE.GeometryUtils.merge(plane, mesh1);
-
-  var mesh2 = new THREE.Mesh( plane2 );
-  mesh2.position.x = mesh1.position.x+1;
-  mesh2.position.y = mesh1.position.y;
-  THREE.GeometryUtils.merge(plane, mesh2);
-
-  var mesh3 = new THREE.Mesh( plane3 );
-  mesh3.position.y = mesh1.position.y+1;
-  mesh3.position.x = mesh1.position.x+1;
-  THREE.GeometryUtils.merge(plane, mesh3);
-
-  var mesh4 = new THREE.Mesh(plane4);
-  mesh4.position.x = mesh1.position.x+2;
-  THREE.GeometryUtils.merge(plane, mesh4);
-
-  mesh = new THREE.Mesh( plane, material );
-
-  return mesh;
-}
-
-
-function CreateZShape()
-{
-  var plane = CreateGeometry();
-  var plane1 = CreateGeometry();
-  var plane2 = CreateGeometry();
-  var plane3 = CreateGeometry();
-  var plane4 = CreateGeometry();
-
-  var material = new THREE.MeshBasicMaterial( {color: 0x00ff00, side: THREE.DoubleSide} ); 
-
-  var mesh1 = new THREE.Mesh( plane1 );
-  mesh1.position.x= 0;
-  mesh1.position.y = 0;
-  THREE.GeometryUtils.merge(plane, mesh1);
-
-  var mesh2 = new THREE.Mesh( plane2 );
-  mesh2.position.x = 1;
-
-  THREE.GeometryUtils.merge(plane, mesh2);
-
-  var mesh3 = new THREE.Mesh( plane3 );
-  mesh3.position.y = -1;
-  mesh3.position.x = 1;
-  THREE.GeometryUtils.merge(plane, mesh3);
-
-  var mesh4 = new THREE.Mesh( plane4 );
-  mesh4.position.y = -1;
-  mesh4.position.x = 2;
-  THREE.GeometryUtils.merge(plane, mesh4);
-
-  mesh = new THREE.Mesh( plane, material );
-
-  return mesh;
-}
-
-
-function CreateSShape()
-{	
-  var plane = CreateGeometry();
-  var plane1 = CreateGeometry();
-  var plane2 = CreateGeometry();
-  var plane3 = CreateGeometry();
-  var plane4 = CreateGeometry();
-
-  var material = new THREE.MeshBasicMaterial( {color: 0x00ff0f, side: THREE.DoubleSide} ); 
-
-  var mesh1 = new THREE.Mesh( plane1 );
-  mesh1.position.x= 0;
-  mesh1.position.y = 0;
-  THREE.GeometryUtils.merge(plane, mesh1);
-
-  var mesh2 = new THREE.Mesh( plane2 );
-  mesh2.position.x = 1;
-
-  THREE.GeometryUtils.merge(plane, mesh2);
-
-  var mesh3 = new THREE.Mesh( plane3 );
-  mesh3.position.y = 1;
-  mesh3.position.x = 1;
-  THREE.GeometryUtils.merge(plane, mesh3);
-
-  var mesh4 = new THREE.Mesh( plane4 );
-  mesh4.position.y = 1;
-  mesh4.position.x = 2;
-  THREE.GeometryUtils.merge(plane, mesh4);
-
-  mesh = new THREE.Mesh( plane, material );
-
-  return mesh;
-}
 
 
 function CreateGeometry()
@@ -265,68 +160,6 @@ function CreateGeometry()
   return geometry;
 }
 
-
-function CreateLineShape()
-{
-  var plane = CreateGeometry();
-  var plane1 = CreateGeometry();
-  var plane2 = CreateGeometry();
-  var plane3 = CreateGeometry();
-  var plane4 = CreateGeometry();
-
-  var material = new THREE.MeshBasicMaterial( {color: 0x0000aa, side: THREE.DoubleSide} ); 
-  var mesh1 = new THREE.Mesh( plane1, material );
-
-  var mesh2 = new THREE.Mesh( plane2, material );
-  mesh2.position.y = mesh1.position.y+1;
-
-  var mesh3 = new THREE.Mesh( plane3, material );
-  mesh3.position.y = mesh2.position.y+1;
-
-  var mesh4 = new THREE.Mesh( plane4, material );
-  mesh4.position.y = mesh3.position.y+1;
-
-  THREE.GeometryUtils.merge(plane, mesh1);
-  THREE.GeometryUtils.merge(plane, mesh2);
-  THREE.GeometryUtils.merge(plane, mesh3);
-  THREE.GeometryUtils.merge(plane, mesh4);
-
-  mesh = new THREE.Mesh( plane, material );
-
-  return mesh;	
-}
-
-
-function CreateSquareShape()
-{
-  var plane = CreateGeometry();
-  var plane1 = CreateGeometry();
-  var plane2 = CreateGeometry();
-  var plane3 = CreateGeometry();
-  var plane4 = CreateGeometry();
-
-  var material = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} ); 
-  var mesh1 = new THREE.Mesh( plane1, material );
-
-  var mesh2 = new THREE.Mesh( plane2, material );
-  mesh2.position.x = mesh1.position.x+1;
-
-  var mesh3 = new THREE.Mesh( plane3, material );
-  mesh3.position.y = mesh2.position.y+1;
-
-  var mesh4 = new THREE.Mesh( plane4, material );
-  mesh4.position.y = mesh1.position.y+1;
-  mesh4.position.x = mesh1.position.x+1;
-
-  THREE.GeometryUtils.merge(plane, mesh1);
-  THREE.GeometryUtils.merge(plane, mesh2);
-  THREE.GeometryUtils.merge(plane, mesh3);
-  THREE.GeometryUtils.merge(plane, mesh4);
-
-  mesh = new THREE.Mesh( plane, material );
-
-  return mesh;
-}
 
 
 function setShape()
@@ -370,7 +203,15 @@ var Tetrominoes = function()
   this.shape = new Shape();
   this.shape.setShape();
 
-
+  this.setState = function(bool)
+  {
+	  this.state=bool;
+  }
+  
+  this.getState = function()
+  {
+	  return state;
+  }
 
   this.changeX = function(x) {
 	this.shape.setX(x);

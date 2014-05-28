091d79bd0caf3d25635e6e62490576744ad7f7a8
Eremeos <Matthias.Bergler@gmx.de>
2014-05-28 21:20:45 +0200

    Levelupbug gefixt

--- Tetris/Game.js
+++ Tetris/Game.js
@@ -2,6 +2,7 @@ var tet;
 var boolgame;
 var speedvalue = 1000;
 var punkte = 0;
+var level = 1;
 
 function game()
 {
@@ -20,24 +21,37 @@ function newTet()
 
 function updatePunkte()
 {
+	
+	if(punkte >= 100)
+		{
+		updateLevel();
+		
+		}
+	else{
 	punkte += 10;
 	document.getElementById("punkte").value = punkte;
+	}
 }
 
 function updateLevel()
 {
-  if(document.getElementById("punkte").value > 100) {
-    document.getElementByID("level").value +=1;
-    document.getElementByID("punkte").value = 0;
-  }
+	punkte = 0;
+	level +=1;
+    document.getElementById("punkte").value = punkte;
+    document.getElementById("level").value = level;
+    updateSchwierigkeit();
+ 
 }
 
 function updateSchwierigkeit()
 {
-  speedvalue -= 100;
+  speedvalue -= 150;
 }
 
 function newGame()
 {
 	window.location.reload();
+	punkte = 0;
+	level = 1;
+	speedvalue= 1000;
 }
